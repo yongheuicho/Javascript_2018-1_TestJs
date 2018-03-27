e1ca73f7a9b202e632a895c06bd9dfba9eb14c09
yongheuicho <yongheuicho@gmail.com>
2018-03-27 12:33:41 +0900

    [2018-03-28] TestJs #5

--- gitComPush.bat
+++ gitComPush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-03-28] TestJs #4"
+git commit -m "[2018-03-28] TestJs #5"
 git push -u origin master
--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,15 @@
-var myHeading = document.querySelector('h1');
-var id = localStorage.getItem('id');
-myHeading.innerHTML = id + ' Homepage';
 function getIdPw() {
     var id = prompt('ID 입력', '');
-    alert(id + '가 로그인함.');
+    var passwoard = prompt(id + '가 사용할 초기 비번 입력', '');
     localStorage.setItem('id', id);
-    var passwordSystem = '12345';
-    var password = prompt('PW 입력', '');
-    if (password === passwordSystem) alert('정상 로그인!');
-    else alert('비밀번호 오류');
+    localStorage.setItem('password', password);
 }
-
-var myImg = document.querySelector('img');
-myImg.onclick = function () {
-    var src = myImg.getAttribute('src');
-    if (src === 'images/firefox-icon.png')
-        myImg.setAttribute('src', 'images/mokwon.png');
-    else
-        myImg.setAttribute('src', 'images/firefox-icon.png');
-}
-
-var myButton = document.querySelector('button');
-myButton.onclick = function () {
+var password = localStorage.getItem('password');
+if (!password) {
     getIdPw();
 }
+// Password?
+
+var id = localStorage.getItem('id');
+var heading = document.querySelector('h1');
+heading.innerHTML = id + '\'s Homepage';
