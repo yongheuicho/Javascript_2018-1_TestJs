95b2ab2361a9191ce59a745462fea4701644675d
yongheuicho <yongheuicho@gmail.com>
2018-03-27 10:45:27 +0900

    [2018-03-28] TestJs #1

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Cloud Service<br>2018-1';
+var text = 'This is a text.';
+text = 10.5;
+var a = 33.7;
+var b = a * text;
+var iceCream = 'chocolate';
+var id = prompt('ID 입력', '');
+alert(id + '가 로그인함.');
