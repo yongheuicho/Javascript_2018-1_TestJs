a6a4783b5cc1a89e2631897502c5545a3881acf4
yongheuicho <yongheuicho@gmail.com>
2018-03-27 11:13:01 +0900

    [2018-03-28] TestJs #2

--- gitComPush.bat
+++ gitComPush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-03-28] TestJs #1"
+git commit -m "[2018-03-28] TestJs #2"
 git push -u origin master
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
 var myHeading = document.querySelector('h1');
 myHeading.innerHTML = 'Cloud Service<br>2018-1';
-var text = 'This is a text.';
-text = 10.5;
-var a = 33.7;
-var b = a * text;
-var iceCream = 'chocolate';
 var id = prompt('ID 입력', '');
 alert(id + '가 로그인함.');
+var passwordSystem = '12345';
+var password = prompt('PW 입력', '');
+if (password === passwordSystem) alert('정상 로그인!');
+else alert('비밀번호 오류');
